edfa4ec623ef6392aa0138855d8164c4c5d29083
achanhon <adrien.chan_hon_tong@onera.fr>
2020-02-29 20:25:50 +0100

    ++

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # invisible data poisonning
 
-## WARNING
+These code highlights the possibility to produce an invisible data poisoning: strongly modify the model (resulting from a fair learning) by adding unperceptible perturbation to training samples.
 
 **THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.**
 
 **The code is provided for research only**
 
-**This code use third party: see the downloaded file!**
+
+## V1 https://www.mdpi.com/2504-4990/1/1/11 - poisonning a frozen deep learning + SVM
+
+**This code use third party**
 
 From theoretic point of view, this code should be fully reproducible.
 
@@ -14,9 +17,11 @@ Yet, it is probably not cross plateform - it has probably hidden dependencies - 
 
 If you do not manadge to reproduce the experiment, feel free to ask me about more detail.
 
-I will try to answer **all** requests depending on my available time.
 
-## DESCRIPTION
+## V2 to appear - poisonning a deep network (with no data augmentation)
 
-This code highlights the possibility to produce an invisible data poisoning: strongly modify the model (resulting from a fair learning) by adding unperceptible perturbation to training samples.
+todo
 
+## V3 in progress - poisonning a deep network
+
+todo
---
+++ V3/generate_fair_model.py
@@ -0,0 +1,80 @@
+#!/opt/anaconda3/bin/python
+
+###
+### some part of the code are inspired from https://github.com/kuangliu/pytorch-cifar
+### 
+import os
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+import collection
+
+print("TRAIN DATA")
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+
+classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
+
+print("MODEL")
+net = torchvision.models.vgg19(pretrained=False, progress=True)
+net.avgpool = nn.Identity()
+net.classifier = None
+net.classifier = nn.Linear(512,2)
+net = net.to(device)
+if device == "cuda":
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+
+print("DEFINE TRAIN")
+losses = collection.deque(200)
+def train(epoch):
+    print("Epoch:", epoch)
+    net.train()
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        outputs = net(inputs)
+        
+        if epoch<150:
+            loss = criterion(outputs, targets)
+        else:
+            loss = 0.1*criterion(outputs, targets)
+            
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        losses.append(loss.cpu().data.numpy())
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        if random.randint(0,10)==0:
+            print(batch_idx,"/",len(trainloader),"loss=",(sum(losses)/len(losses)),"train accuracy=",100.*correct/total)
+
+    if epoch%50==49:
+        torch.save("fairmodel.pth",net.state_dict())
+
+print("MAIN")    
+for epoch in range(300):
+    train(epoch)
+torch.save("fairmodel.pth",net.state_dict())
