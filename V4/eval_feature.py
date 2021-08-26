eeef3da7c21001d973318ea8e2c5d55d05c78112
teletravail <adrienchanhonton@gmail.com>
2021-08-26 20:31:19 +0200

    +++

--- V4/eval_feature.py
+++ V4/eval_feature.py
@@ -1,24 +1,25 @@
 import torch
 
 
-def sizeclassicaldataset(name,train):
-    if name=="mnist" and train:
+def sizeclassicaldataset(name, train):
+    if name == "mnist" and train:
         return 60000
-    if name=="mnist" and not train:
+    if name == "mnist" and not train:
         return 10000
-        
-    if name=="cifar10" and train:
+
+    if name == "cifar10" and train:
         return 50000
-    if name=="cifar10" and not train:
+    if name == "cifar10" and not train:
         return 10000
-        
-    if name=="svhn" and train:
+
+    if name == "svhn" and train:
         return 73257
-    if name=="svhn" and not train:
+    if name == "svhn" and not train:
         return 26032
-        
+
     print("unknown dataset")
     quit()
+
 
 def compute_accuracy(batchprovider, encoder, classifier, datasetsize, device="cuda"):
     with torch.no_grad():
@@ -26,14 +27,51 @@ def compute_accuracy(batchprovider, encoder, classifier, datasetsize, device="cu
         for x, y in batchprovider:
             x, y = x.to(device), y.to(device)
             feature = encoder(x)
-            _,z = (classifier(feature)).max(1)
+            _, z = (classifier(feature)).max(1)
             accuracy.append((y == z).float().sum())
         accuracy = torch.Tensor(accuracy).sum() / datasetsize
         return accuracy.cpu().detach().numpy()
 
-import random
+
+from sklearn import svm
+import numpy
+
 
 def train_frozenfeature_classifier(
+    batchprovider, encoder, datasetsize, featuredim, nbclasses, device="cuda"
+):
+    print("extract features")
+    X = numpy.zeros((datasetsize, featuredim))
+    Y = numpy.zeros(datasetsize)
+
+    with torch.no_grad():
+        i = 0
+        for x, y in batchprovider:
+            x, y = x.to(device), y.to(device)
+            feature = encoder(x).cpu().numpy()
+            lenx = x.shape[0]
+            X[i : i + lenx] = feature
+            Y[i : i + lenx] = y
+            i += lenx
+
+    print("solve SVM", datasetsize, featuredim)
+    classifier = svm.LinearSVC()
+    classifier.fit(X, Y)
+
+    print("extract torch classifier")
+    classifierNN = torch.nn.Linear(featuredim, nbclasses)
+    classifierNN.weight = torch.transpose(torch.Tensor(coef_[i]), 0, 1)
+    classifierNN.bias = torch.Tensor(intercept_)
+
+    print(
+        "accuracy",
+        compute_accuracy(batchprovider, encoder, classifier, datasetsize, device),
+    )
+    return classifier
+
+
+#only for problem to large to fit in RAM ?
+def train_frozenfeature_classifier_SGD(
     batchprovider,
     encoder,
     datasetsize,
@@ -60,8 +98,6 @@ def train_frozenfeature_classifier(
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            #if random.randint(0,30)==0:
-            #    print(loss.cpu().detach().numpy())
 
         accuracy = compute_accuracy(
             batchprovider, encoder, classifier, datasetsize, device=device
@@ -106,7 +142,9 @@ if __name__ == "__main__":
 
     print("train classifier on the top of the encoder")
 
-    classifier = train_frozenfeature_classifier(trainloader, net, sizeclassicaldataset("mnist",True), 512, 10)
+    classifier = train_frozenfeature_classifier(
+        trainloader, net, sizeclassicaldataset("mnist", True), 512, 10
+    )
 
     print("eval classifier")
     testset = torchvision.datasets.MNIST(
@@ -116,4 +154,9 @@ if __name__ == "__main__":
         trainset, batch_size=64, shuffle=True, num_workers=2
     )
 
-    print("accuracy = ", compute_accuracy(testloader, net, classifier, sizeclassicaldataset("mnist",False)))
+    print(
+        "accuracy = ",
+        compute_accuracy(
+            testloader, net, classifier, sizeclassicaldataset("mnist", False)
+        ),
+    )
