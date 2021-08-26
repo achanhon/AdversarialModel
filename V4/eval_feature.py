cc5c4a748b5d5a05a375bd1eb1b7285056b33628
teletravail <adrienchanhonton@gmail.com>
2021-08-26 17:42:40 +0200

    ++

--- V4/eval_feature.py
+++ V4/eval_feature.py
@@ -1,18 +1,37 @@
 import torch
 
 
+def sizeclassicaldataset(name,train):
+    if name=="mnist" and train:
+        return 60000
+    if name=="mnist" and not train:
+        return 10000
+        
+    if name=="cifar10" and train:
+        return 50000
+    if name=="cifar10" and not train:
+        return 10000
+        
+    if name=="svhn" and train:
+        return 73257
+    if name=="svhn" and not train:
+        return 26032
+        
+    print("unknown dataset")
+    quit()
+
 def compute_accuracy(batchprovider, encoder, classifier, datasetsize, device="cuda"):
     with torch.no_grad():
         accuracy = []
         for x, y in batchprovider:
             x, y = x.to(device), y.to(device)
             feature = encoder(x)
-            z = classifier(feature)
-            z, _ = torch.max(z, dim=1)
-            accuracy.append((y == z).float()).sum()
+            _,z = (classifier(feature)).max(1)
+            accuracy.append((y == z).float().sum())
         accuracy = torch.Tensor(accuracy).sum() / datasetsize
         return accuracy.cpu().detach().numpy()
 
+import random
 
 def train_frozenfeature_classifier(
     batchprovider,
@@ -25,7 +44,7 @@ def train_frozenfeature_classifier(
     device="cuda",
     earlystopping=True,
 ):
-    classifier = torch.nn.Linear(featuredim, nbclasses)
+    classifier = torch.nn.Linear(featuredim, nbclasses).to(device)
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(classifier.parameters(), lr=lr, momentum=0.6)
 
@@ -41,6 +60,8 @@ def train_frozenfeature_classifier(
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            #if random.randint(0,30)==0:
+            #    print(loss.cpu().detach().numpy())
 
         accuracy = compute_accuracy(
             batchprovider, encoder, classifier, datasetsize, device=device
@@ -51,7 +72,7 @@ def train_frozenfeature_classifier(
     return classifier
 
 
-if __name__ == __main__:
+if __name__ == "__main__":
     print("example of usage")
     import torchvision
     import os
@@ -62,34 +83,37 @@ if __name__ == __main__:
     os.system("mv data build")
 
     print("import dataset")
+    transform = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.Resize((32, 32)),
+            torchvision.transforms.Grayscale(num_output_channels=3),
+            torchvision.transforms.ToTensor(),
+        ]
+    )
+
     trainset = torchvision.datasets.MNIST(
-        root="./build/data",
-        train=True,
-        download=True,
-        transform=torchvision.transforms.ToTensor(),
+        root="./build/data", train=True, download=True, transform=transform
     )
     trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=128, shuffle=True, num_workers=2
+        trainset, batch_size=64, shuffle=True, num_workers=2
     )
 
     print("import pretrained model")
     net = torchvision.models.vgg13(pretrained=True)
     net.avgpool = torch.nn.Identity()
     net.classifier = torch.nn.Identity()
+    net.cuda()
 
     print("train classifier on the top of the encoder")
 
-    train_frozenfeature_classifier(trainloader, net, 40000, 512, 10)
+    classifier = train_frozenfeature_classifier(trainloader, net, 40000, 512, 10)
 
     print("eval classifier")
     testset = torchvision.datasets.MNIST(
-        root="./build/data",
-        train=False,
-        download=True,
-        transform=torchvision.transforms.ToTensor(),
+        root="./build/data", train=False, download=True, transform=transform
     )
     testloader = torch.utils.data.DataLoader(
-        trainset, batch_size=128, shuffle=True, num_workers=2
+        trainset, batch_size=64, shuffle=True, num_workers=2
     )
 
     print("accuracy = ", compute_accuracy(testloader, net, classifier, 10000))
