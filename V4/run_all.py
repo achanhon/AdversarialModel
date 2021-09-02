8c6600295568fb547860b99039f0c43e8877d934
teletravail <adrienchanhonton@gmail.com>
2021-09-02 14:47:44 +0200

    comme il écrase au fur et à mesure :-(

--- V4/run_all.py
+++ V4/run_all.py
@@ -7,10 +7,12 @@ whereIam = os.uname()[1]
 assert whereIam in ["ldti719z", "wdtim719z"]
 
 if whereIam == "ldti719z":
-    os.system("python3 -u cifar.py | tee build/cifar.log")
-    os.system("python3 -u mnist.py | tee build/mnist.log")
-    os.system("python3 -u frogcifar.py | tee build/frogcifar.log")
+    os.system("python3 -u cifar.py | tee cifar.log")
+    os.system("python3 -u mnist.py | tee mnist.log")
+    os.system("python3 -u frogcifar.py | tee frogcifar.log")
 else:
-    os.system("/data/anaconda3/bin/python -u cifar.py | tee build/cifar.log")
-    os.system("/data/anaconda3/bin/python -u mnist.py | tee build/mnist.log")
-    os.system("/data/anaconda3/bin/python -u frogcifar.py | tee build/frogcifar.log")
+    os.system("/data/anaconda3/bin/python -u cifar.py | tee cifar.log")
+    os.system("/data/anaconda3/bin/python -u mnist.py | tee mnist.log")
+    os.system("/data/anaconda3/bin/python -u frogcifar.py | tee frogcifar.log")
+
+os.system("mv *.log build")
