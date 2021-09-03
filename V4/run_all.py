29f4524007d62fdcb6f53fc7d6bfcfa949afb3b2
teletravail <adrienchanhonton@gmail.com>
2021-09-03 13:14:51 +0200

    ++

--- V4/run_all.py
+++ V4/run_all.py
@@ -4,9 +4,9 @@ os.system("rm -r build __pycache__")
 os.system("mkdir build")
 
 whereIam = os.uname()[1]
-assert whereIam in ["ldti706z", "wdtim719z", "super"]
+assert whereIam in ["ldtis706z", "wdtim719z", "super"]
 
-if whereIam == "ldti706z":
+if whereIam == "ldtis706z":
     os.system("python3 -u cifar.py | tee cifar.log")
     os.system("python3 -u mnist.py | tee mnist.log")
     os.system("python3 -u frogcifar.py | tee frogcifar.log")
