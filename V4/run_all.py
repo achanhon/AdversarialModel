7cc7fd2eadf4f32cf542a402ce4dbc7a90152cf1
teletravail <adrienchanhonton@gmail.com>
2021-09-03 13:13:08 +0200

    performance hyper differente entre wtdis719z et ldtis706z ????

--- V4/run_all.py
+++ V4/run_all.py
@@ -4,9 +4,9 @@ os.system("rm -r build __pycache__")
 os.system("mkdir build")
 
 whereIam = os.uname()[1]
-assert whereIam in ["ldti719z", "wdtim719z"]
+assert whereIam in ["ldti706z", "wdtim719z", "super"]
 
-if whereIam == "ldti719z":
+if whereIam == "ldti706z":
     os.system("python3 -u cifar.py | tee cifar.log")
     os.system("python3 -u mnist.py | tee mnist.log")
     os.system("python3 -u frogcifar.py | tee frogcifar.log")
