e393020d8077e6610a92fb5212d847627ac99974
achanhon <adrien.chan_hon_tong@onera.fr>
2021-09-06 10:12:42 +0200

    ça remarche enfin - bizarre ces bugs quand même

--- V4/run_all.py
+++ V4/run_all.py
@@ -10,9 +10,9 @@ if whereIam == "ldtis706z":
     os.system("python3 cifar.py")
     os.system("python3 mnist.py")
     os.system("python3 frogcifar.py")
-    os.system("python3 frogcifar.py")
+    os.system("python3 trainnoise.py")
 else:
     os.system("/data/anaconda3/bin/python cifar.py")
     os.system("/data/anaconda3/bin/python mnist.py")
     os.system("/data/anaconda3/bin/python frogcifar.py")
-    os.system("/data/anaconda3/bin/python frogcifar.py")
+    os.system("/data/anaconda3/bin/python trainnoise.py")
