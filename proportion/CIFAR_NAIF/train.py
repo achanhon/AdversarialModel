f44c65a44ea263061765d385e89c1b017c7a883c
teletravail <adrienchanhonton@gmail.com>
2022-06-29 21:42:16 +0200

    ++

--- proportion/CIFAR_NAIF/train.py
+++ proportion/CIFAR_NAIF/train.py
@@ -12,52 +12,46 @@ else:
     print("no cuda")
     quit()
 
-raw = torchvision.transforms.ToTensor()
-
 print("load data")
-trainset = torchvision.datasets.CIFAR10(
-    root="./build/data", train=True, download=True, transform=raw
-)
+raw = torchvision.transforms.ToTensor()
+root, Tr = "./build/data", True
+trainset = torchvision.datasets.CIFAR10(root=root, train=Tr, download=Tr, transform=raw)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True)
 
 print("load model")
-import torch.nn as nn
-
-net = torchvision.models.vgg13(pretrained=True)
-net.avgpool = nn.Identity()
-net.classifier = None
-net.classifier = nn.Linear(512, 10)
-net = net.to(device)
-if device == "cuda":
-    torch.cuda.empty_cache()
-    cudnn.benchmark = True
+RESNET = True
+if RESNET:
+    net = torchvision.models.resnet50(pretrained=True)
+    net.avgpool = torch.nn.Identity()
+    net.classifier = nn.Linear(2048, 10)
+else:
+    net = torchvision.models.vgg16(pretrained=True)
+    net.avgpool = torch.nn.Identity()
+    net.classifier = nn.Linear(512, 10)
+net = net.cuda()
+net.train()
 
 print("train setting")
-import torch.optim as optim
-import collections
-import random
-from sklearn.metrics import confusion_matrix
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.001)
-
-meanloss = collections.deque(maxlen=200)
-nbepoch = 150
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+batchsize = 32
+nbepoch = 10
 
 print("train")
 for epoch in range(nbepoch):
     print("epoch=", epoch, "/", nbepoch)
     net.train()
-    total, correct = 0, 0
-    for _, (inputs, targets) in enumerate(trainloader):
+    total, correct = torch.zeros(1).cuda(), torch.zeros(1).cuda()
+    printloss = torch.zeros(2).cuda()
+    for inputs, targets in trainloader:
         inputs, targets = inputs.to(device), targets.to(device)
 
         outputs = net(inputs)
         loss = criterion(outputs, targets)
+        printloss[0] += loss.detach()
+        printloss[1] += batchsize
 
-        meanloss.append(loss.cpu().data.numpy())
-
-        if epoch > 75:
+        if epoch > 5:
             loss *= 0.1
 
         optimizer.zero_grad()
@@ -66,14 +60,14 @@ for epoch in range(nbepoch):
         optimizer.step()
 
         _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
+        total += batchsize
+        correct += (predicted == targets).float().sum()
 
-        if random.randint(0, 30) == 0:
-            print("loss=", (sum(meanloss) / len(meanloss)))
+        if printloss[1] > 500:
+            print("loss=", printloss[0] / printloss[1])
+            printloss = torch.zeros(2).cuda()
 
     torch.save(net, "build/model.pth")
     print("train accuracy=", 100.0 * correct / total)
     if correct > 0.98 * total:
         quit()
-    sleep(3)
