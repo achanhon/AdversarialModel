febe7f8ec36e4939c9d2d2aee7aed6901deaedab
teletravail <adrienchanhonton@gmail.com>
2022-06-29 22:02:55 +0200

    +

--- proportion/CIFAR_NAIF/go.sh
+++ proportion/CIFAR_NAIF/go.sh
@@ -3,5 +3,5 @@ rm -r build
 mkdir build
 mv data build
 
-/data/anaconda3/bin/python -u train.py | tee build/train.log
-/data/anaconda3/bin/python -u test.py | tee build/test.log
+/d/jcastillo/anaconda3/bin/python -u train.py | tee build/train.log
+/d/jcastillo/anaconda3/bin/python -u test.py | tee build/test.log
