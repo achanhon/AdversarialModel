6b82a38220b4d5311ca66f2254b250f7c2c3aa79
teletravail <adrienchanhonton@gmail.com>
2022-06-30 17:00:46 +0200

    détruisons un peu la planète ;-)

--- proportion/CIFAR_NAIF/go.sh
+++ proportion/CIFAR_NAIF/go.sh
@@ -4,4 +4,16 @@ mkdir build
 mv data build
 
 /d/jcastillo/anaconda3/bin/python -u train.py | tee build/train.log
-/d/jcastillo/anaconda3/bin/python -u test.py | tee build/test.log
+/d/jcastillo/anaconda3/bin/python -u test.py | tee build/test1.log
+
+/d/jcastillo/anaconda3/bin/python -u train.py | tee build/train.log
+/d/jcastillo/anaconda3/bin/python -u test.py | tee build/test2.log
+
+/d/jcastillo/anaconda3/bin/python -u train.py | tee build/train.log
+/d/jcastillo/anaconda3/bin/python -u test.py | tee build/test3.log
+
+/d/jcastillo/anaconda3/bin/python -u train.py | tee build/train.log
+/d/jcastillo/anaconda3/bin/python -u test.py | tee build/test4.log
+
+/d/jcastillo/anaconda3/bin/python -u train.py | tee build/train.log
+/d/jcastillo/anaconda3/bin/python -u test.py | tee build/test5.log
