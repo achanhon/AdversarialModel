ad82f42a3c8a2d63437d6a238617bc442469da5a
teletravail <adrienchanhonton@gmail.com>
2022-07-01 20:37:49 +0200

    +

---
+++ proportion/CIFAR_NAIF/wtf.py
@@ -0,0 +1,7 @@
+import torch
+
+torch.save(torch.rand(1), "build/" + "vgg13_1.txt")
+torch.save(torch.rand(1), "build/" + "vgg13_2.txt")
+torch.save(torch.rand(1), "build/" + "vgg13_3.txt")
+torch.save(torch.rand(1), "build/" + "vgg13_4.txt")
+torch.save(torch.rand(1), "build/" + "vgg13_5.txt")
