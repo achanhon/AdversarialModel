7ce3aaf7ddbae6cce2025b4735aedac8bb9487b3
teletravail <adrienchanhonton@gmail.com>
2022-07-01 11:11:45 +0200

    un premier résultat très positif !

--- proportion/proportionloss/go.sh
+++ proportion/proportionloss/go.sh
@@ -17,3 +17,13 @@ mv data build
 
 /d/jcastillo/anaconda3/bin/python -u train.py | tee build/train.log
 /d/jcastillo/anaconda3/bin/python -u test.py | tee build/test5.log
+
+
+#"resnet34"
+#0.1327
+#0.0968
+#0.1093
+#0.1457
+#0.0870
+#0.1143  (+/- 0.000600265)
+
--- proportion/twohead/go.sh
+++ proportion/twohead/go.sh
@@ -17,3 +17,12 @@ mv data build
 
 /d/jcastillo/anaconda3/bin/python -u train.py | tee build/train.log
 /d/jcastillo/anaconda3/bin/python -u test.py | tee build/test5.log
+
+
+#"resnet34"
+#0.1657
+#0.1649
+#0.1615
+#0.1648
+#0.1749
+#0.1663  (+/- 0,000025388)
