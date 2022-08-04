4669cac51ff4603dbc42f08cd6f6fa66e67145ab
achanhon <adrien.chan_hon_tong@onera.fr>
2022-08-04 15:04:21 +0200

    ++

---
+++ proportion/SVHN/fair/density.py
@@ -0,0 +1,40 @@
+import torch
+
+
+def logitTOdensity(logit):
+    softmaxdensity = torch.nn.functional.softmax(logit, dim=1)
+    tmp = torch.nn.functional.relu(logit) + softmaxdensity
+    total = tmp.sum(dim=1)
+    total = torch.stack([total] * logit.shape[1], dim=1)
+
+    estimatedensity = tmp / total
+    return estimatedensity.sum(dim=0) / logit.shape[0]
+
+
+def labelsTOdensity(targets, nbclasses=10):
+    truedensity = torch.zeros(nbclasses).cuda()
+    for i in range(nbclasses):
+        truedensity[i] = (targets == i).float().sum() / targets.shape[0]
+    return truedensity
+
+
+def extendedKL(estimatedensity, truedensity):
+    kl_loss = torch.nn.KLDivLoss(reduction="sum")
+    kl = kl_loss(estimatedensity, truedensity)
+    kl = torch.nn.functional.relu(kl)
+    diff = estimatedensity - truedensity
+    return kl + torch.sum(diff * diff) + diff.abs().sum()
+
+
+def weightedlogitTOdensity(logit, weight):
+    softmaxdensity = torch.nn.functional.softmax(logit, dim=1)
+    tmp = torch.nn.functional.relu(logit) + softmaxdensity
+    total = tmp.sum(dim=1)
+    total = torch.stack([total] * logit.shape[1], dim=1)
+
+    estimatedensity = tmp / total
+    estimatedensity = estimatedensity / weight
+
+    estimatedensity = estimatedensity.sum(dim=0)
+    total = estimatedensity.sum(dim=0)
+    return estimatedensity / total
