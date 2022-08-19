3e2f3529d3016e167ca71a081ebb7b39f070ee66
achanhon <adrien.chan_hon_tong@onera.fr>
2022-08-19 14:51:59 +0200

    tout ça à faire :-(

---
+++ proportion/MDVD/selective/density.py
@@ -0,0 +1,80 @@
+import torch
+import torchvision
+
+
+def logitTOdensity(logit):
+    softmaxdensity = torch.nn.functional.softmax(logit, dim=1)
+    tmp = torch.nn.functional.relu(logit) + softmaxdensity
+    total = tmp.sum(dim=1)
+    total = torch.stack([total] * logit.shape[1], dim=1)
+
+    estimatedensity = tmp / total
+    return estimatedensity.sum(dim=0) / logit.shape[0]
+
+
+def labelsTOdensity(targets, nbclasses=10):
+    truedensity = torch.zeros(nbclasses).cuda()
+    for i in range(nbclasses):
+        truedensity[i] = (targets == i).float().sum() / targets.shape[0]
+    return truedensity
+
+
+def extendedKL(estimatedensity, truedensity):
+    kl_loss = torch.nn.KLDivLoss(reduction="sum")
+    kl = kl_loss(estimatedensity, truedensity)
+    kl = torch.nn.functional.relu(kl)
+    diff = estimatedensity - truedensity
+    return kl + torch.sum(diff * diff) + diff.abs().sum()
+
+
+class EurosatSplit(torch.utils.data.Dataset):
+    def __init__(self, flag):
+        assert flag in ["train", "test"]
+        self.flag = flag
+
+        Tr, Pa = True, "build/data"
+        if flag == "test":
+            tmp = [torchvision.transforms.Resize(32), torchvision.transforms.ToTensor()]
+        else:
+            tmp = [
+                torchvision.transforms.RandomResizedCrop(32),
+                torchvision.transforms.RandomRotation(90),
+                torchvision.transforms.RandomHorizontalFlip(0.5),
+                torchvision.transforms.RandomVerticalFlip(0.5),
+                torchvision.transforms.ToTensor(),
+            ]
+        aug = torchvision.transforms.Compose(tmp)
+
+        self.alldata = torchvision.datasets.EuroSAT(root=Pa, download=Tr, transform=aug)
+
+        if self.flag == "train":
+            self.size = len(self.alldata) * 2 // 3 - 2
+        else:
+            self.size = len(self.alldata) // 3 - 2
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        if self.flag == "test":
+            return self.alldata.__getitem__(idx * 3 + 2)
+        else:
+            lol = 3 * (idx // 2) + (idx % 2)  # thank oeis
+            return self.alldata.__getitem__(lol)
+
+
+def selectivelogitTOdensity(logit):
+    entropy = torch.sum(-logit * torch.log(logit + 0.000001), dim=1)
+    entropyIndex = [(entropy[i], i) for i in range(entropy.shape[0])]
+    entropyIndex = sorted(entropyIndex)
+    selectedIndex = [entropyIndex[i][1] for i in range(int(0.8 * entropy.shape[0]))]
+    tmp = [logit[i] for i in selectedIndex]
+    logit = torch.stack(tmp)
+
+    softmaxdensity = torch.nn.functional.softmax(logit, dim=1)
+    tmp = torch.nn.functional.relu(logit) + softmaxdensity
+    total = tmp.sum(dim=1)
+    total = torch.stack([total] * logit.shape[1], dim=1)
+
+    estimatedensity = tmp / total
+    return estimatedensity.sum(dim=0) / logit.shape[0]
---
+++ proportion/MDVD/selective/test.py
@@ -0,0 +1,76 @@
+import sys
+import torch
+import torchvision
+import density
+
+if torch.cuda.is_available():
+    torch.cuda.empty_cache()
+    torch.backends.cudnn.benchmark = True
+else:
+    print("no cuda")
+    quit()
+if len(sys.argv) == 1:
+    print("no output provided")
+    quit()
+
+if len(sys.argv) > 2:
+    print("just merge result")
+    outputpath = sys.argv[1]
+    inputpaths = sys.argv[2:]
+    inputs = [torch.load("build/" + name) for name in inputpaths]
+    print(inputs)
+    inputs = torch.Tensor(inputs)
+    mean = inputs.mean()
+    var = inputs.var()
+    meanvar = str(mean.cpu().numpy()) + "  (\pm " + str(var.cpu().numpy()) + ")"
+    with open(outputpath, "w") as f:
+        f.write(meanvar)
+    f.close()
+    print(mean, var)
+    quit()
+
+
+print("load data")
+Bs = 256
+testset = density.EurosatSplit("test")
+testloader = torch.utils.data.DataLoader(testset, batch_size=Bs, shuffle=True)
+
+print("load model")
+net = torch.load("build/model.pth")
+net = net.cuda()
+net.eval()
+
+with torch.no_grad():
+    print("classical test")
+    cm = torch.zeros(10, 10).cuda()
+    for inputs, targets in testloader:
+        inputs, targets = inputs.cuda(), targets.cuda()
+
+        outputs = net(inputs)
+        _, predicted = outputs.max(1)
+
+        for i in range(10):
+            for j in range(10):
+                cm[i][j] += torch.sum((targets == i).float() * (predicted == j).float())
+
+    total = torch.sum(cm)
+    accuracy = torch.sum(torch.diagonal(cm)) / total
+    print("test cm", cm)
+    print("test accuracy=", accuracy)
+
+    print("proportion test")
+    averageKL = torch.zeros(2).cuda()
+    for epoch in range(10):
+        for inputs, targets in testloader:
+            inputs, targets = inputs.cuda(), targets.cuda()
+
+            outputs = net(inputs)
+
+            estimatedensity = density.selectivelogitTOdensity(outputs)
+            truedensity = density.labelsTOdensity(targets)
+            averageKL[0] += density.extendedKL(estimatedensity, truedensity)
+            averageKL[1] += 1
+
+    averageKL = averageKL[0] / averageKL[1]
+    torch.save(averageKL, "build/" + sys.argv[1])
+    print("test divergence=", averageKL)
