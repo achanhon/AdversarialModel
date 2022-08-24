6c2878d612eb36e2a0d4ae035bb8fd6dd4840c70
achanhon <adrien.chan_hon_tong@onera.fr>
2022-08-24 14:41:10 +0200

    ++

---
+++ proportion/PASCAL/posterior/density.py
@@ -0,0 +1,112 @@
+import os
+import PIL
+from PIL import Image
+import torch
+import torchvision
+
+
+def smooth(vect):
+    return torch.nn.functional.avg_pool1d(
+        vect.unsqueeze(0), kernel_size=15, padding=7, stride=1
+    )[0]
+
+
+def normalize(positive_vect):
+    total = positive_vect.sum()
+    return positive_vect / total
+
+
+def logitTOdensity(logit, sizes):
+    sizes = torch.sqrt(sizes).int()
+    density = torch.zeros(200).cuda()
+
+    weight1 = torch.nn.functional.softmax(logit, dim=1)[:, 1] - 0.5
+    weigth2 = torch.nn.functional.relu(logit)
+    weigth2 = weigth2[:, 1] / (weigth2[:, 1] + weigth2[:, 0] + 0.01)
+
+    for i in range(sizes.shape[0]):
+        if weight1[i] > 0:
+            density[sizes[i]] = weight1[i] + weigth2[i] + density[sizes[i]]
+
+    return normalize(smooth(density))
+
+
+def selectivelogitTOdensity(logit, sizes):
+    sizes = torch.sqrt(sizes).int()
+    density = torch.zeros(200).cuda()
+
+    weight1 = torch.nn.functional.softmax(logit, dim=1)[:, 1] - 0.5
+    weigth2 = torch.nn.functional.relu(logit)
+    weigth2 = weigth2[:, 1] / (weigth2[:, 1] + weigth2[:, 0] + 0.01)
+
+    kept = []
+    for i in range(weight1.shape[0]):
+        if weight1[i] > 0.5:
+            kept.append((weight1[i] - 0.5, i))
+    kept = sorted(kept)
+    kept = kept[int(0.2 * len(kept)) :]
+
+    for i in kept:
+        density[sizes[i]] = weight1[i] + weigth2[i] + density[sizes[i]]
+
+    return normalize(smooth(density))
+
+
+def labelsT0density(targets, sizes):
+    sizes = torch.sqrt(sizes).int()
+    density = torch.zeros(200).cuda()
+
+    for i in range(sizes.shape[0]):
+        if targets[i] == 1:
+            density[sizes[i]] = 1 + density[sizes[i]]
+
+    return normalize(smooth(density))
+
+
+def extendedKL(estimatedensity, truedensity):
+    kl_loss = torch.nn.KLDivLoss(reduction="sum")
+    kl = kl_loss(estimatedensity, truedensity)
+    kl = torch.nn.functional.relu(kl)
+    diff = estimatedensity - truedensity
+    return kl + torch.sum(diff * diff) + diff.abs().sum()
+
+
+class MDVD(torch.utils.data.Dataset):
+    def __init__(self, flag):
+        assert flag in ["train", "test"]
+        self.flag = flag
+        self.root = "../selectivesearch/build/MDVD/" + flag
+        if flag == "test":
+            tmp = [
+                torchvision.transforms.Resize((32, 32)),
+                torchvision.transforms.ToTensor(),
+            ]
+        else:
+            tmp = [
+                torchvision.transforms.Resize((32, 32)),
+                torchvision.transforms.RandomRotation(90),
+                torchvision.transforms.RandomHorizontalFlip(0.5),
+                torchvision.transforms.RandomVerticalFlip(0.5),
+                torchvision.transforms.ToTensor(),
+            ]
+        self.aug = torchvision.transforms.Compose(tmp)
+
+        self.sizeP = len(os.listdir(self.root + "/good"))
+        self.sizeN = len(os.listdir(self.root + "/bad"))
+
+    def __len__(self):
+        return self.sizeP + self.sizeN
+
+    def __getitem__(self, idx):
+        if idx < self.sizeN:
+            path = self.root + "/bad/" + str(idx) + ".png"
+            label = 0
+        else:
+            path = self.root + "/good/" + str(idx - self.sizeN) + ".png"
+            label = 1
+        image = PIL.Image.open(path).convert("RGB").copy()
+
+        size = image.size[0] * image.size[0] + image.size[1] * image.size[1]
+        if size >= 200 * 200:
+            size = 200 * 200 - 1
+        return self.aug(image), label, size
---
+++ proportion/PASCAL/posterior/test.py
@@ -0,0 +1,90 @@
+import sys
+import torch
+import torchvision
+import density
+
+if torch.cuda.is_available():
+    torch.cuda.empty_cache()
+    torch.backends.cudnn.benchmark = True
+else:
+    print("no cuda")
+    quit()
+if len(sys.argv) == 1:
+    print("no output provided")
+    quit()
+
+if len(sys.argv) > 2:
+    print("just merge result")
+    outputpath = sys.argv[1]
+    inputpaths = sys.argv[2:]
+    inputs = [torch.load("build/" + name) for name in inputpaths]
+    print(inputs)
+    inputs = torch.Tensor(inputs)
+    mean = inputs.mean()
+    var = inputs.var()
+    meanvar = str(mean.cpu().numpy()) + "  (\pm " + str(var.cpu().numpy()) + ")"
+    with open(outputpath, "w") as f:
+        f.write(meanvar)
+    f.close()
+    print(mean, var)
+    quit()
+
+
+print("load data")
+Bs = 256
+testset = density.MDVD("test")
+testloader = torch.utils.data.DataLoader(testset, batch_size=Bs, shuffle=True)
+
+print("load model")
+net = torch.load("build/model.pth")
+weights = torch.load("build/model_externalweigths.pth")
+net = net.cuda()
+net.eval()
+
+with torch.no_grad():
+    print("classical test")
+    cm = torch.zeros(2, 2).cuda()
+    for inputs, targets, _ in testloader:
+        inputs, targets = inputs.cuda(), targets.cuda()
+
+        outputs = net(inputs)
+        _, predicted = outputs.max(1)
+
+        for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+            cm[i][j] += torch.sum((targets == i).float() * (predicted == j).float())
+
+    total = torch.sum(cm)
+    accuracy = torch.sum(torch.diagonal(cm)) / total
+    print("test cm", cm)
+    print("test accuracy=", accuracy)
+
+    print("proportion test")
+    averageKL = torch.zeros(2).cuda()
+    averageKLsele = torch.zeros(2).cuda()
+    averageKLfair = torch.zeros(2).cuda()
+    for epoch in range(10):
+        for inputs, targets, sizes in testloader:
+            inputs, targets, sizes = inputs.cuda(), targets.cuda(), sizes.cuda()
+
+            outputs = net(inputs)
+
+            estimatedensity = density.logitTOdensity(outputs, sizes)
+            withrejection = density.selectivelogitTOdensity(outputs, sizes)
+            withfairness = estimatedensity.clone() * weights
+            withfairness = normalize(withfairness)
+
+            truedensity = density.labelsT0density(targets, sizes)
+            averageKL[0] += density.extendedKL(estimatedensity, truedensity)
+            averageKL[1] += 1
+            averageKLsele[0] += density.extendedKL(withrejection, truedensity)
+            averageKLsele[1] += 1
+            averageKLfair[0] += density.extendedKL(withfairness, truedensity)
+            averageKLfair[1] += 1
+
+    averageKL = averageKL[0] / averageKL[1]
+    averageKLselected = averageKLselected[0] / averageKLselected[1]
+    averageKLfair = averageKLfair[0] / averageKLfair[1]
+    torch.save(averageKL, "build/baseline_" + sys.argv[1])
+    torch.save(averageKLselected, "build/selective_" + sys.argv[1])
+    torch.save(averageKLfair, "build/fair_" + sys.argv[1])
+    print("test divergence=", averageKL, averageKLselected, averageKLfair)
