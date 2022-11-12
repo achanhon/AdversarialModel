04a066216964e98b3ed02193bfdd6cd1b503412f
achanhon <adrienchanhonton@gmail.com>
2022-11-12 21:05:38 +0100

    +

--- proportion/eurosat/proportionloss/test.py
+++ proportion/eurosat/proportionloss/test.py
@@ -31,9 +31,10 @@ if len(sys.argv) > 2:
 
 
 print("load data")
-#Bs = 256
-Bs = 128 ### experiments requested by reviewers
-Bs = 512 ### experiments requested by reviewers
+# Bs = 256
+Bs = 128  ### experiments requested by reviewers
+Bs = 512  ### experiments requested by reviewers
+BS = 256
 testset = density.EurosatSplit("test")
 testloader = torch.utils.data.DataLoader(testset, batch_size=Bs, shuffle=True)
 
